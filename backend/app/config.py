b5eb30e95751445f3d1ba02e8daf94fd4c7c883e
Rafik Kouissar <rafikkouissar@Rafiks-MBP-2.fios-router.home>
2025-01-09 22:50:38 -0500

    minor updates

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    GROQ_API_KEY: str = "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"
+    GROQ_API_KEY: str = ""
     CHROMA_PERSISTENCE_DIR: str = "./chroma_db"
-    MODEL_NAME: str = "llama3-8b-8192"
+    MODEL_NAME: str = "mixtral-8x7b-32768"
     
     class Config:
         env_file = ".env"
